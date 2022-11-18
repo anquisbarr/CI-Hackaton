d3b997e26ac81753af1e2f3f4a5f7f17363818e4
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2022-11-17 19:57:21 -0500

    Added OrderUpdate button and functionality in orders/index.ts

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import { Box, Stack, Text } from "@chakra-ui/layout";
@@ -7,48 +8,54 @@ import { trpc } from "../utils/trpc";
 import { CreateProductOrderInput } from "../schema/product-order.schema";
 
 export const OrderForm = () => {
-    const { handleSubmit, register } = useForm<CreateProductOrderInput>();
-    const router = useRouter();
-  
-    const { mutate, error}  = trpc.useMutation(["orders.createOrder"])
-  
-    function onSubmit(values: CreateProductOrderInput) {
-      mutate(values);
-    }
-  
-    return (
-      <>
-        <Stack alignItems={"center"}>
-          <Box textAlign="center" mt={2}>
-            <Text fontSize={"2xl"} fontWeight="bold" textColor={"purple.500"}>
-              Agregar un nuevo producto
-            </Text>
-          </Box>
-          <Box>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <FormControl>
-                <FormLabel>Código de producto</FormLabel>
-                <Input type="text" {...register(`productId`)} />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Cantidad de producto</FormLabel>
-                <Input type="text" {...register(`quantity`)} />
-              </FormControl>
-              <button>
-                <Button colorScheme="purple" width="full" mt={3}>
-                  Crear nuevo producto
-                </Button>
-              </button>
-              <FormControl>
-                <Text>
-                  {error?.message}
+  const { handleSubmit, register } = useForm<CreateProductOrderInput>();
+  const [success, setSuccess] = useState(false);
+  const router = useRouter();
+
+  const { mutate, error } = trpc.useMutation(["orders.createOrder"], {
+    onSuccess: () => {
+      setSuccess(true);
+    },
+  });
+
+  function onSubmit(values: CreateProductOrderInput) {
+    mutate(values);
+  }
+
+  return (
+    <>
+      <Stack alignItems={"center"}>
+        <Box textAlign="center" mt={2}>
+          <Text fontSize={"2xl"} fontWeight="bold" textColor={"purple.500"}>
+            Agregar un nuevo producto
+          </Text>
+        </Box>
+        <Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl>
+              <FormLabel>Código de producto</FormLabel>
+              <Input type="text" {...register(`productId`)} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Cantidad de producto</FormLabel>
+              <Input type="text" {...register(`quantity`)} />
+            </FormControl>
+            <button>
+              <Button colorScheme="purple" width="full" mt={3}>
+                Crear nuevo producto
+              </Button>
+            </button>
+            <FormControl>
+              <Text>
+                {success && `Se ha creado la orden con éxito`}
+                {error?.message}
                 </Text>
-              </FormControl>
-            </form>
-          </Box>
-        </Stack>
-      </>
-    );
-  };
+            </FormControl>
+          </form>
+        </Box>
+      </Stack>
+    </>
+  );
+};
 
 export default OrderForm;
--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,55 +1,98 @@
+import { useState } from "react";
 import { trpc } from "../../utils/trpc";
 import { Flex, VStack, HStack, Box } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { TableContainer, Table, Thead, Tbody, Tr, Td } from "@chakra-ui/table";
-import NextLink from "next/link";
+import { Select } from "@chakra-ui/select";
+import { UpdateProductOrderInput } from "../../schema/product-order.schema";
+import Loading from "../../components/Loading";
+
+type STATUS = "PENDING" | "CONFIRMED" | "ACTIVE" | "DELIVERED";
 
 const OrdersListingPage = () => {
   const { data, isLoading } = trpc.useQuery(["orders.all"]);
+  const {
+    mutate,
+    isLoading: isUpdating,
+    error,
+  } = trpc.useMutation("orders.update-status");
+  const [selected, setSelected] = useState("");
 
-  console.log(data);
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.target.value);
+  };
 
+  const handleOrderStatusList = (status: string) => {
+    const statusList = ["PENDING", "CONFIRMED", "ACTIVE", "DELIVERED"];
+    if (statusList.includes(status)) {
+      let itemToRemove = statusList.indexOf(status);
+      statusList.splice(itemToRemove, 1);
+      return statusList;
+    }
+    return statusList;
+  };
+
+  const handleUpdateStatus = async (values: UpdateProductOrderInput) => {
+    mutate(values);
+  };
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <Loading />;
   }
 
   return (
     <VStack mt={5}>
-        <HStack>
-            <Box ml={500}>
-            <NextLink href='/orders/update'>
-                <Button colorScheme={'purple'}>Actualizar orden</Button>
-            </NextLink>
-            </Box>
-        </HStack>
-    <Flex alignItems={"center"} textAlign={'center'}>
-      <TableContainer>
-        <Table>
-          <Thead>
-            <Tr>
-              <th>Código de Orden</th>
-              <th>Código Usuario</th>
-              <th>Estado</th>
-              <th>Total </th>
-            </Tr>
-          </Thead>
-          <Tbody textAlign={'center'}>
-            {data?.map((order) => {
-              return (
-                <Tr key={order.id}>
-                  <Td>{order.id}</Td>
-                  <Td>{order.userId}</Td>
-                  <Td>{order.status}</Td>
-                  <Td>{order.total} PEN</Td>
-                </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </Flex>
-
+      <Flex alignItems={"center"} textAlign={"center"}>
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <th>Código de Orden</th>
+                <th>Código Usuario</th>
+                <th>Estado</th>
+                <th>Total </th>
+              </Tr>
+            </Thead>
+            <Tbody textAlign={"center"}>
+              {data?.map((order) => {
+                return (
+                  <Tr key={order.id}>
+                    <Td>{order.id}</Td>
+                    <Td>{order.userId}</Td>
+                    <Td>{order.status}</Td>
+                    <Td>{order.total} PEN</Td>
+                    <Td>
+                      <Select onChange={(e) => handleSelect(e)}>
+                        {handleOrderStatusList(order.status).map((status) => {
+                          return (
+                            <option key={status} value={status}>
+                              {status}
+                            </option>
+                          );
+                        })}
+                      </Select>
+                    </Td>
+                    <Td>
+                      <Button
+                        colorScheme={"purple"}
+                        disabled={isUpdating}
+                        onClick={() =>
+                          handleUpdateStatus({
+                            orderId: order.id,
+                            status: selected as STATUS,
+                          })
+                        }
+                      >
+                        Actualizar
+                      </Button>
+                    </Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Flex>
     </VStack>
   );
 };
