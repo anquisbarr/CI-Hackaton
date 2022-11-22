f2d8c7190acc4649d88e44e7d8ca84bde6d985c1
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2022-11-21 21:33:44 -0500

    hotfix: deleted unnecesarry files

--- src/server/route/app.router.ts
+++ src/server/route/app.router.ts
@@ -1,5 +1,4 @@
 import { createRouter } from "../createRouter";
-import { postRouter } from "./post.router";
 import { userRouter } from "./user.router";
 import { categoryRouter } from "./category.router";
 import { productRouter } from "./product.router";
@@ -7,7 +6,6 @@ import { orderRouter } from "./order.router"
 
 export const appRouter = createRouter()
     .merge('users.', userRouter)
-    .merge('posts.', postRouter)
     .merge('categories.', categoryRouter)
     .merge('products.', productRouter)
     .merge('orders.',orderRouter)
